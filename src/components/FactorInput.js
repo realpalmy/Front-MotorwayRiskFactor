92d56d87b3924651739ff4a6b51fb6e033b149da
Nicha Satthamnuwong <nichasatt@Nichas-MBP.attlocal.net>
2024-05-13 14:29:15 -0700

    fix year value ddl

--- src/components/FactorInput.js
+++ src/components/FactorInput.js
@@ -139,7 +139,7 @@ export default function Factor2022() {
                 <div className="col-span-2 content-end">
                   <select
                     id="selectYear"
-                    value={2567}
+                    defaultValue={2567}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     onChange={() =>
                       setYear(document.getElementById("selectYear").value)
