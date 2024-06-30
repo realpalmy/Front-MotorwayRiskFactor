fcab80effa286e5ae2ec223a90e1cd6476335094
Nicha Satthamnuwong <nichasatt@Nichas-MBP.attlocal.net>
2024-06-30 03:49:43 -0700

    operation plan and data

---
+++ src/components/OperationPlanTable.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+
+const PlanTable = ({ operationData }) => {
+    const trow_css = " py-4 sm:p-0 flex flex-col sm:table-row "
+    const thead_css = " w-full sm:w-1/4 sm:py-4 pb-1 sm:pe-2 text-gray-600 font-medium align-top "
+    const tdata_css = " w-full sm:w-3/4 sm:py-4 align-top "
+
+    return (
+        operationData && 
+        <div className="w-full inline-block align-top">
+            <div className="overflow-x-auto rounded-lg">
+                <table className="min-w-ful table-auto divide-y divide-gray-200">
+                    <tbody className="divide-y divide-gray-200">
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                งาน / โครงการ / กิจกรรม / กระบวนการปฏิบัติงาน:
+                            </td>
+                            <td className={tdata_css + "whitespace-pre-line"}>
+                                {operationData.project}
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                ปัจจัยความเสี่ยง / ปัจจัยเสี่ยงหรือสาเหตุของความเสี่ยงที่เหลืออยู่ (1)
+                            </td>
+                            <td className={tdata_css}>
+                                {operationData.risk}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className={"py-4 pe-0 text-gray-800 font-semibold"} colSpan={2}>
+                                ขั้นตอนการบริหารความเสี่ยง
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                วิธีจัดการความเสี่ยง (2)
+                            </td>
+                            <td className={tdata_css}>
+                                {operationData.solution} <span className='underline underline-offset-auto font-semibold'>{operationData.solution_underline}</span>
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                กิจกรรมการจัดการความเสี่ยง (3)
+                            </td>
+                            <td className={tdata_css + "whitespace-pre-wrap"}>
+                                <ul className={operationData.activity?.length > 1 ? "list-disc" : "list-none"}>
+                                    {operationData.activity?.map((list, i) => <li key={i} className="whitespace-pre-line">{list}</li>)}
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                ผลที่คาดว่าจะได้รับ (4)
+                            </td>
+                            <td className={tdata_css + "whitespace-pre-line"}>
+                                <ul className={operationData.result?.length > 1 ? "list-disc" : "list-none"}>
+                                    {operationData.result?.map((list, i) => <li key={i} className="whitespace-pre-line">{list}</li>)}
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                ระยะเวลาการดำเนินการ (5)
+                            </td>
+                            <td className={tdata_css}>
+                                {operationData.duration}
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                ผู้รับผิดชอบ (6)
+                            </td>
+                            <td className={tdata_css}>
+                                {operationData.responsibleBy}
+                            </td>
+                        </tr>
+                        <tr className={trow_css}>
+                            <td className={thead_css}>
+                                งบประมาณ
+                            </td>
+                            <td className={tdata_css}>
+                                {operationData.budget}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+const ResidualRiskTable = ({before, dataset}) => {
+    return (
+        dataset && <div>
+        <table
+            className="table-fixed max-w-full border rounded-lg text-center text-sm border-gray-200">
+            <thead className="border-b font-medium border-gray-200">
+                <tr>
+                    <td className="text-center px-6 py-4 font-[550]" colSpan={3}>{before == true ? "ความรุนแรงก่อนการบริหาร" : "ความรุนแรงที่องค์กรยอมรับได้"}</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="border-b">
+                    <td className="w-[8rem] whitespace-nowrap border-r px-3 py-4">โอกาส</td>
+                    <td className="w-1/3 whitespace-nowrap border-r px-3 py-4">ผลกระทบ</td>
+                    <td className="w-1/3 whitespace-nowrap px-3 py-4">ระดับความเสี่ยง</td>
+                </tr>
+                <tr className="border-b">
+                    <td className="border-r px-6 py-4">{dataset.l}</td>
+                    <td className="border-r px-6 py-4">{dataset.i}</td>
+                    <td className="px-6 py-4">{parseInt(dataset.l) * parseInt(dataset.i)}</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+    )
+}
+
+const ResidualRisk = ({riskDataset}) => {
+    return (
+        riskDataset && <div className="flex flex-col sm:flex-row gap-3">
+            <div>
+                <ResidualRiskTable before={true} dataset={riskDataset.before}></ResidualRiskTable>
+            </div>
+            <div>
+                <ResidualRiskTable before={false} dataset={riskDataset.organization}></ResidualRiskTable>
+            </div>
+        </div>
+    )
+}
+
+export {PlanTable, ResidualRisk}
---
+++ src/components/SearchOperation.js
@@ -0,0 +1,96 @@
+import { Fragment, useState, useContext, useEffect } from "react";
+import { PlanTable, ResidualRisk } from "./OperationPlanTable";
+import { plan2566 } from "../data/operation_plan/plan2566";
+import { plan2567 } from "../data/operation_plan/plan2567";
+import { plan2568 } from "../data/operation_plan/plan2568";
+
+export default function SearchOrganization() {
+
+  const [year, setYear] = useState("")
+  const [operationDataSet, setOperationDataSet] = useState([]);
+
+  const searchForm = (event) => {
+    event.preventDefault();
+    const year = document.getElementById("year").value;
+    setYear(year)
+    setOperationDataSet(yearData(year))
+  };
+
+  const yearData = (year) => {
+    if(year === '2566') 
+      return plan2566
+    else if (year === '2567') 
+      return plan2567
+    else if (year === '2568') 
+      return plan2568
+    else
+      return '';
+  }
+
+
+  return (
+    <Fragment>
+      <div className="container mx-auto my-6 px-2">
+        <div className="p-2 sm:p-0">
+          <h1 className="text-xl font-semibold text-center sm:text-start sm:pb-1">
+            ค้นหาแผนปฏิบัติการรองรับความเสี่ยงประจำปี
+          </h1>
+
+          <div className="flex flex-col justify-start">
+            <form onSubmit={searchForm} className="flex justify-start mb-6">
+              <div className="grid grid-cols-2 md:grid-cols-5 gap-4 w-full">
+                <div className="col-span-2 content-end">
+                  <label
+                    htmlFor="year"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    ปี
+                  </label>
+                  <select
+                    id="year"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    <option value="">ระบุ ปี</option>
+                    <option value="2566">ปี 2566</option>
+                    <option value="2567">ปี 2567</option>
+                    <option value="2568">ปี 2568</option>
+                  </select>
+                </div>
+                <div className="grid content-end col-span-2 md:col-span-1">
+                  <button
+                    type="submit"
+                    className="py-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  >
+                       ค้นหา
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            {year !== '' && 
+            
+            <Fragment>
+                <div id="search_result" className="flex flex-col sm:flex-row sm:justify-between">
+                    <div className="flex w-full">
+                        <h2 className="font-semibold text-lg">แผนปฏิบัติการรองรับความเสี่ยงของเงินทุนค่าธรรมเนียมผ่านทาง ประจำปี {year}</h2>
+                    </div>
+                </div>
+            
+                {operationDataSet?.length > 0 && operationDataSet.map((operationData) => 
+                  <div key={operationData.title} className="flex-col border mt-4 p-3 sm:p-5 rounded">
+                    <h3 className="font-semibold text-indigo-500">{operationData.title}</h3>
+                    <PlanTable operationData={operationData}></PlanTable>
+
+                    <p className="my-4 font-semibold">การพิจารณาระดับความเสี่ยงที่ยอมรับได้ของความเสี่ยงที่เหลืออยู่ (Residual Risk) ขององค์กร</p>
+                    <ResidualRisk riskDataset={operationData.residualRisk}></ResidualRisk>
+                </div>
+                )}
+            </Fragment>
+            }
+            
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
